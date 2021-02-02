5aa8fed69fb6cbd9687de382d9a0853e59404198
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2021-02-02 10:27:26 +0300

    Add transactions

--- rust-blockchain/src/main.rs
+++ rust-blockchain/src/main.rs
@@ -3,80 +3,118 @@ use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 use chrono::prelude::*;
 
-#[derive(Clone)]
+#[derive(Debug, Clone, Hash)]
+struct Transaction {
+    payload: String
+}
+
+
+#[derive(Debug, Clone)]
 struct Block {
     index: u64,
     timestamp: i64,
     hash: u64,
     prev_block_hash: u64,
-    payload: String,
+    transactions: Vec<Transaction>,
 }
 
+#[derive(Debug)]
 struct BlockChainError {
-    message: &'static str
+    message: String
 }
 
+#[derive(Debug)]
 struct BlockChain {
-    chain: RefCell<Vec<Block>>,
+    chain: Vec<Block>,
+    pending_transactions: Vec<Transaction>,
 }
+
+/*impl fmt::Debug for BlockChain {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list()
+            .entries(self.chain.borrow().iter())
+            .finish()
+    }
+}*/
 
 impl BlockChain {
+    fn generate_genesis_block() -> Block {
+        let utc: DateTime<Utc> = Utc::now();
+        return Block {
+            index: 0,
+            timestamp: utc.timestamp(),
+            hash: 0,
+            transactions: vec![],
+            prev_block_hash: 0,
+        };
+    }
+
     fn new(genesis_block: Block) -> BlockChain {
         BlockChain {
-            chain: RefCell::new(vec![genesis_block])
+            chain: vec![genesis_block],
+            pending_transactions: vec![],
         }
     }
 
-    fn add_block(self: &BlockChain, new_block: Block) -> Result<(), BlockChainError> {
+    pub fn get_last_block(&self) -> Block {
+        let block_optional = self.chain.last();
+        block_optional.unwrap().clone()
+    }
+
+    pub fn send_transaction(&mut self, transaction: Transaction) {
+        self.pending_transactions.push(transaction)
+    }
+
+    //Mine new block with all pending transactions in it
+    pub fn mine(&mut self) -> Block {
+        let prev_block = self.get_last_block();
+        let transactions = &mut vec![];
+        for transaction in self.pending_transactions.drain(..) {
+            transactions.push(transaction)
+        }
+        let new_block = self.generate_block(prev_block.index + 1, prev_block.hash, transactions.to_owned());
+        self.add_block(new_block.clone());
+        new_block
+    }
+
+    //private
+    fn generate_block(&self, index: u64, prev_block_hash: u64, transactions: Vec<Transaction>) -> Block {
+        let utc: DateTime<Utc> = Utc::now();
+        let timestamp = utc.timestamp();
+        let mut hasher = DefaultHasher::new();
+        index.hash(&mut hasher);
+        timestamp.hash(&mut hasher);
+        transactions.iter().for_each(|tr| tr.hash(&mut hasher));
+        prev_block_hash.hash(&mut hasher);
+        let hash = hasher.finish();
+        return Block {
+            index,
+            timestamp,
+            hash,
+            transactions,
+            prev_block_hash: 0,
+        };
+    }
+
+    //private
+    fn add_block(&mut self, new_block: Block) {
         let last_block = self.get_last_block();
         if new_block.prev_block_hash == last_block.hash {
-            self.chain.borrow_mut().push(new_block);
-            Ok(())
+            self.chain.push(new_block);
         } else {
-            Err(BlockChainError { message: "Block should refer to the latest block in the chain" })
+            panic!("Block should refer to the latest block in the chain")
         }
     }
-
-    fn get_last_block(self: &BlockChain) -> &Block {
-        let chain = self.chain.borrow();
-        chain.last().unwrap()
-    }
 }
 
-fn generate_genesis_block(payload: String) -> Block {
-    let index: u64 = 0;
-    let prev_block_hash: u64 = 0;
-    generate_block(index, prev_block_hash, payload)
-}
-
-fn generate_next_block(prev_block: &Block, payload: String) -> Block {
-    generate_block(prev_block.index + 1, prev_block.hash, payload)
-}
-
-fn generate_block(index: u64, prev_block_hash: u64, payload: String) -> Block {
-    let utc: DateTime<Utc> = Utc::now();
-    let timestamp = utc.timestamp();
-    let mut hasher = DefaultHasher::new();
-    index.hash(&mut hasher);
-    timestamp.hash(&mut hasher);
-    payload.hash(&mut hasher);
-    prev_block_hash.hash(&mut hasher);
-    let hash = hasher.finish();
-    return Block {
-        index,
-        timestamp,
-        hash,
-        payload,
-        prev_block_hash: 0,
-    };
-}
 
 fn main() {
-    let genesis_block = generate_genesis_block(String::from("block_0_payload"));
-    let blockchain = BlockChain::new(genesis_block);
+    let genesis_block = BlockChain::generate_genesis_block();
+    let blockchain = &mut BlockChain::new(genesis_block);
 
-    let last_block = blockchain.get_last_block();
-    let new_block = generate_next_block(last_block, String::from("block_1_payload"));
-    blockchain.add_block(new_block);
+    blockchain.send_transaction(Transaction { payload: "payload".to_string() });
+    blockchain.mine();
+
+    println!("{:?}", blockchain);
 }
 
