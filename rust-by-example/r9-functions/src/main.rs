a633989e1b37361ad2c6c3119a573d638cc17fdc
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2021-10-17 18:34:50 +0300

    Functions

--- rust-by-example/r9-functions/src/main.rs
+++ rust-by-example/r9-functions/src/main.rs
@@ -97,6 +97,94 @@ fn methods() {
 fn closures() {
     println!("\n# closures");
 
+    fn inc(i: i32) -> i32 { i + 1 }
+    let inc_closure_annotated = |i: i32| -> i32 { i + 1 };
+    let inc_closure_inferred = |i| i + 1;
+
+    let i = 1;
+    println!("inc: {}", inc(i));
+    println!("inc_closure_annotated: {}", inc_closure_annotated(i));
+    println!("inc_closure_inferred: {}", inc_closure_inferred(i));
+
+    // A closure taking no arguments which returns an `i32`.
+    // The return type is inferred.
+    let one = || 1;
+    println!("closure returning one: {}", one());
+
+    {
+        // # Capturing
+        use std::mem;
+
+        let color = String::from("green");
+
+        // A closure to print `color` which immediately borrows (`&`) `color` and
+        // stores the borrow and closure in the `print` variable. It will remain
+        // borrowed until `print` is used the last time.
+        //
+        // `println!` only requires arguments by immutable reference so it doesn't
+        // impose anything more restrictive.
+        let print = || println!("`color`: {}", color);
+
+        let _reborrow = &color;
+        print();
+
+        let mut count = 0;
+        // A closure to increment `count` could take either `&mut count` or `count`
+        // but `&mut count` is less restrictive so it takes that. Immediately
+        // borrows `count`.
+        //
+        // A `mut` is required on `inc` because a `&mut` is stored inside. Thus,
+        // calling the closure mutates the closure which requires a `mut`.
+        let mut inc = || {
+            count += 1;
+            println!("`count`: {}", count);
+        };
+
+        // Call the closure using a mutable borrow.
+        inc();
+
+        // The closure still mutably borrows `count` because it is called later.
+        // An attempt to reborrow will lead to an error.
+        // let _reborrow = &count;
+        // ^ TODO: try uncommenting this line.
+        inc();
+
+        // The closure no longer needs to borrow `&mut count`. Therefore, it is
+        // possible to reborrow without an error
+        let _count_reborrowed = &mut count;
+
+
+        // A non-copy type.
+        let movable = Box::new(3);
+
+        // `mem::drop` requires `T` so this must take by value. A copy type
+        // would copy into the closure leaving the original untouched.
+        // A non-copy must move and so `movable` immediately moves into
+        // the closure.
+        let consume = || {
+            println!("`movable`: {:?}", movable);
+            mem::drop(movable);
+        };
+
+        // `consume` consumes the variable so this can only be called once.
+        consume();
+        // consume();
+        // ^ TODO: Try uncommenting this line.
+
+        // `Vec` has non-copy semantics.
+        let haystack = vec![1, 2, 3];
+
+        // Using move before vertical pipes forces closure to take ownership of captured variables:
+        let contains = move |needle| haystack.contains(needle);
+
+        println!("{}", contains(&1));
+        println!("{}", contains(&4));
+
+        // println!("There're {} elements in vec", haystack.len());
+        // ^ Uncommenting above line will result in compile-time error
+        // because borrow checker doesn't allow re-using variable after it
+        // has been moved.
+    }
 }
 
 fn high_order_function() {
