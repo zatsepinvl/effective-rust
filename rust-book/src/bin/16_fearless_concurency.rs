f57cb526a957cf60f713ad1abf1d29780af99092
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2022-01-02 19:54:33 +0300

    Add channels

--- rust-book/src/bin/16_fearless_concurency.rs
+++ rust-book/src/bin/16_fearless_concurency.rs
@@ -1,7 +1,36 @@
+use std::sync::mpsc::{Receiver, Sender};
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
+    //threads();
+    channels();
+}
+
+fn channels() {
+    let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got {}", received);
+    }
+}
+
+fn threads() {
+
     let v = vec![1, 2, 3, 4, 5];
     thread::spawn(move || {
         println!("Worker: {:?}", v);
@@ -9,3 +38,4 @@ fn main() {
         println!("Worker: finish");
     }).join();
 }
+
