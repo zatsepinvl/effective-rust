7abaa22c097868544fc13fe8b6300f0a619e7284
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2022-10-01 12:45:30 +0200

    Start scoping rules

---
+++ rust-by-example/src/bin/15_scoping_rules.rs
@@ -0,0 +1,60 @@
+fn main() {
+    raii();
+    ownership_and_moves();
+}
+
+
+fn raii() {
+    // Rust enforces RAII (Resource Acquisition Is Initialization)
+    // https://en.wikipedia.org/wiki/Resource_acquisition_is_initialization
+    //  so whenever an object goes out of scope, its destructor is called and its owned resources are freed.
+
+    {
+        // Allocate an integer on the heap
+        let _box = Box::new(3);
+
+        // `_box1` is destroyed here, and memory gets freed
+    }
+
+    // The notion of a destructor in Rust is provided through the Drop trait.
+    struct ToDrop;
+
+    impl Drop for ToDrop {
+        fn drop(&mut self) {
+            println!("ToDrop is being dropped");
+        }
+    }
+
+    let _drop = ToDrop;
+}
+
+fn ownership_and_moves() {
+    // resource can have ONLY ONE owner
+    // Stack variables
+    // _Stack_ allocated integer
+    let x = 5u32;
+
+    // *Copy* `x` into `y` - no resources are moved
+    let y = x;
+
+    // Both values can be independently used
+    println!("x is {}, and y is {}", x, y);
+
+    // Heap variables
+    // `a` is a pointer to a _heap_ allocated integer
+    let a = Box::new(5i32);
+    println!("a contains: {}", a);
+    // *Move* `a` into `b`
+    let b = a;
+    // Error! `a` can no longer access the data, because it no longer owns the
+    // heap memory
+    //println!("a contains: {}", a);
+}
+
+fn borrowing() {
+    // ToDo
+}
+
+fn lifetimes() {
+    // ToDo
+}
