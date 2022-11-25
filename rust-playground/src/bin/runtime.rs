75e58a95f13d8c0a14dd671a31c5afc41785ada5
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2022-11-25 22:16:30 +0100

    Add runtime example

---
+++ rust-playground/src/bin/runtime.rs
@@ -0,0 +1,42 @@
+use std::marker::PhantomData;
+use crate::module_a::Pallet;
+
+fn main() {
+    struct Runtime;
+    struct ConsoleLogger;
+
+    impl Logger for ConsoleLogger {
+        fn log() {
+            println!("Log1")
+        }
+    }
+
+    impl module_a::Config for Runtime {
+        type MyLogger = ConsoleLogger;
+    }
+
+    let pallet: Pallet<Runtime> = Pallet(PhantomData);
+    pallet.do_something();
+}
+
+
+trait Logger {
+    fn log();
+}
+
+mod module_a {
+    use std::marker::PhantomData;
+    use crate::Logger;
+
+    pub trait Config {
+        type MyLogger: Logger;
+    }
+
+    pub struct Pallet<T>(pub PhantomData<T>);
+
+    impl<T: Config> Pallet<T> {
+        pub fn do_something(&self) {
+            T::MyLogger::log();
+        }
+    }
+}
